e7798e2479ca19b1f88c054462c62f9d1552493e
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-10-06 16:44:31 +0600

    card title and authors name done with api

--- scripts/video.js
+++ scripts/video.js
@@ -28,11 +28,18 @@ const displayVideos = (videos) => {
       class = "h-full w-full object-cover"
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-2 py-3 flex gap-2">
+    <div>
+        <img class="w-10 h-10 rounded-full object-cover" src = "${video.authors[0].profile_picture}" />
+    </div>
+
+    <div>
+    <h2 class="font-bold"> ${video.title} </h2>
+    <div class ="flex items-center gap-2">
+        <p class = "text-gray-400"> ${video.authors[0].profile_name} </p>
+        <img class="w-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+    </div>
+    <p> </p>
     </div>
   </div>
         `;
